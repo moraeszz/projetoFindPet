adbe3b8d98f06595bdf284c0f99bd4077c8f0aec
Melissa <melissamelo529@gmail.com>
2021-09-21 11:23:05 -0300

    estruturação de pastas

---
+++ findpet-app/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import { Container } from "./styles";
+
+function Header() {
+    return(
+        <Container>
+
+        </Container>
+    );
+}
+
+export default Header;
---
+++ findpet-app/src/components/Header/styles.js
@@ -0,0 +1,7 @@
+export const Container = {  
+    width: '100vw',
+    height: '60px',
+    position: 'fixed',
+    top: 0,
+    backgroundColor: '#000000'
+}
